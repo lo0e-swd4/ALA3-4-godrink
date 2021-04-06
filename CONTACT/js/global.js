56876b6ab1a303a5a329d8114befed5d0de7a32f
MNH225 <74601920+MNH225@users.noreply.github.com>
2021-04-06 13:54:20 +0200

    Contact pagina

---
+++ CONTACT/js/global.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Mobiel version//
+function openBtn(){
+    document.getElementById("menu").style.width = "100vw"
+}
+
+function closeBtn(){
+    document.getElementById("menu").style.width = "0"
+}
